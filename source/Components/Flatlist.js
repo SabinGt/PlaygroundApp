4d4e9a991d17b8a31aa71cb6d31878e3dc7a25b7
unknown <sabingt0@gmail.com>
2022-06-15 12:40:38 +0545

    flatlist added

--- source/Components/Flatlist.js
+++ source/Components/Flatlist.js
@@ -1,13 +1,37 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text, StyleSheet, StatusBar, FlatList} from 'react-native';
+import React from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+const DATA = [
+  {
+    id: '1',
+    title: 'Bread',
+  },
+  {
+    id: '2',
+    title: 'Beer',
+  },
+  {
+    id: '3',
+    title: 'Diaper',
+  },
+  {
+    id: '4',
+    title: 'Milk',
+  },
+];
 
-const Flatlist = ({navigation, route}) => {
+const Item = ({title}) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
+const FlatlistDetails = ({navigation, route}) => {
   const {color, icon, title} = route.params;
-
+  const renderItem = ({item}) => <Item title={item.title} />;
   return (
     <View>
-  <View
+      <View
         style={{
           display: 'flex',
           flexDirection: 'row',
@@ -21,13 +45,37 @@ const Flatlist = ({navigation, route}) => {
           style={{
             fontSize: 20,
             fontWeight: '500',
+            color: 'white',
           }}>
           {title}
         </Text>
         <FontAwesomeIcon icon={icon} size={30}></FontAwesomeIcon>
       </View>
+      <View style={styles.container}>
+        <FlatList
+          data={DATA}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+        />
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Flatlist
+export default FlatlistDetails;
+const styles = StyleSheet.create({
+  container: {
+    height: 600,
+  },
+  item: {
+    backgroundColor: 'green',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 32,
+    color: 'white',
+  },
+});
