f17796b33f99bba9fde6a1d64f75d8f4ef63da9a
unknown <sabingt0@gmail.com>
2022-06-15 11:41:22 +0545

    icons added to icon page

--- source/Components/Icon.js
+++ source/Components/Icon.js
@@ -1,13 +1,14 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text} from 'react-native';
+import React from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
-
+import {faHomeUser} from '@fortawesome/free-solid-svg-icons';
+import {faCar} from '@fortawesome/free-solid-svg-icons';
 const Icon = ({navigation, route}) => {
   const {color, icon, title} = route.params;
 
   return (
     <View>
-     <View
+      <View
         style={{
           display: 'flex',
           flexDirection: 'row',
@@ -26,8 +27,20 @@ const Icon = ({navigation, route}) => {
         </Text>
         <FontAwesomeIcon icon={icon} size={30}></FontAwesomeIcon>
       </View>
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-evenly',
+          alignItems: 'center',
+          height: 600,
+        }}>
+        <FontAwesomeIcon icon={faHomeUser} size={50} color="red" />
+        <FontAwesomeIcon icon={icon} size={50} color="purple" />
+        <FontAwesomeIcon icon={faCar} size={50} color="green" />
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Icon
+export default Icon;
