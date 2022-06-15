4d492e931b8c47970fc61aea5e16f078d99f722e
unknown <sabingt0@gmail.com>
2022-06-15 11:03:20 +0545

    checkbox added

--- source/Components/Checkbox.js
+++ source/Components/Checkbox.js
@@ -1,13 +1,15 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text} from 'react-native';
+import React, {useState} from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
-
+import CheckBox from '@react-native-community/checkbox';
 const Checkbox = ({navigation, route}) => {
+  const [toggleCheckBox, setToggleCheckBox] = useState(false);
+
   const {color, icon, title} = route.params;
 
   return (
     <View>
-     <View
+      <View
         style={{
           display: 'flex',
           flexDirection: 'row',
@@ -26,8 +28,16 @@ const Checkbox = ({navigation, route}) => {
         </Text>
         <FontAwesomeIcon icon={icon} size={30}></FontAwesomeIcon>
       </View>
+      <View>
+        <CheckBox
+          disabled={false}
+          value={toggleCheckBox}
+          onValueChange={newValue => setToggleCheckBox(newValue)}
+        />
+        <Text>{toggleCheckBox ? 'Checked' : 'Unchecked'}</Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Checkbox
+export default Checkbox;
